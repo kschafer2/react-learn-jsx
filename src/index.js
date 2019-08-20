dbc166c3de1b3d24752f346df37ffdd35d6f1cac
Kyle Schafer <kschay@protonmail.com>
2019-08-20 14:39:11 -0500

    Closes #3 - Referenced Function getButtonText in JSX

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+  return 'Click On Me!';
+}
+
 // Create a react component
 const App = () => {
     return (
       <div>
-<<<<<<< HEAD
         <label className="label" for="name">
-=======
-        <label class="label" for="name">
->>>>>>> c0c5a0e33627b77e51090bf8e1d0221ccd745cbc
           Enter name:
         </label>
         <input id="name" type="text"/>
         <button style={{ backgroundColor: 'blue', color: 'white' }}>
-          Submit
+          {getButtonText()}
         </button>
       </div>
     );
